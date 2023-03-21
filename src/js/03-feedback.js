74b0b30db0be8ba97f2efb2a5aeb7f8e95af255b
Dan Dudaiev <roffrooff@gmail.com>
2023-03-21 18:06:22 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+const lodash = require('lodash')
 
+const feedbackForm = document.querySelector('.feedback-form');
+
+const FEEDBACK_DATA_KEY = localStorage.getItem('feedback-form-state');
+
+if (FEEDBACK_DATA_KEY) {
+  const receivedFeedback = JSON.parse(FEEDBACK_DATA_KEY);
+
+  feedbackForm.email.value = receivedFeedback.email;
+  feedbackForm.message.value = receivedFeedback.message;
+}
+
+
+feedbackForm.addEventListener('input', lodash.throttle(() => {
+  const feedbackData = {
+    email: feedbackForm.email.value,
+    message: feedbackForm.message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackData));
+}, 500));
+
+
+feedbackForm.addEventListener('submit', ev => {
+  ev.preventDefault();
+
+  const feedbackFormValues = {
+    email: feedbackForm.email.value,
+    message: feedbackForm.message.value,
+  };
+
+  localStorage.clear();
+  feedbackForm.reset();
+
+  console.log(feedbackFormValues);
+});
