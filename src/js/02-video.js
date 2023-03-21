f5de7a7e27c5b9846c363290c6a2a6cf8be93b2d
Dan Dudaiev <roffrooff@gmail.com>
2023-03-21 17:28:23 +0200

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+
+const lodash = require('lodash');
+
+const iframe = document.querySelector('iframe');
+const vimeoPlayer = new Player(iframe);
+
+vimeoPlayer.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+
+vimeoPlayer.on('timeupdate', lodash.throttle((ev) => {
+    localStorage.setItem('videoplayer-current-time', `${ev.seconds}`)
+}, 1000))
+
 
